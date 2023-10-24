30d251f8cc1518fe7aed7d9e1bee6f5d3fbf5cb2
fayruza117 <fbobomurodova073@gmail.com>
2023-10-24 10:07:13 +0300

    js da vazifa

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// alert("Hello Hammaga"); // single line
+//conform("JS ni o'rganishga tayyormisiz?");
+//prompt("Parol kirirting: ");
+//console.log("Hammaga Salom!");
+// const max_life = 3;
+// console.log(max_life);
+// max_life = 4;
+// let age = 19;
+// console.log(age);
+// age = 20;
+// console.log(age);
+// let fName = propmt ("Ismingizni kiriting: "):
+// let lName = prompt("Familiyangizni kiriting: ");
+
+// console.log(fName + " " + lName);
+
+// let input = prompt("Ismingizni kirirting: ");
+
+// console.log(input);
+
+// data types
+// Primitive
+
+// String
+// let name = "Eshmat
+// hsrtgdjgjgjdhdhgghdh
+//\n O'zbekiston";
+// Number
+// let age = 18;
+// let price = 999.99;
+// Boolean
+// let isGood = true;
+// let isBad = false;
+// undefined => aniqlanmagan
+// let motto = undefined;
+// console.log(motto);
+// null => bo'sh
+// let favSport = null;
+// console.log(favSport);
+
+// Reference => Bogliqlik
+// Array => to'plam
+let ism = prompt("Ismingiz nima?: ");
+let familiya = prompt("Familyangiz nima?: ");
+let uqish_joyingiz = prompt("Qayerda uqiysiz?: ");
+let yoshingiz = prompt("Yoshingiz nechchida?: ");
+let kasbingiz = prompt("Nima ishqilasz?: ");
+let hobby = prompt ("Nimalarga qiziqasiz?: ");
+
+console.log("Ismi: "  + ism  + " familyasi: " + familiya + " O'qish joyi: " + uqish_joyingiz + " yoshi: " + yoshingiz + " kasbi: " + kasbingiz);
